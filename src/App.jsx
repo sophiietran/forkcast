a70a5bc65f04fa30e53e35a7800b705bc1a28216
Sophie <sophiie.tran@gmail.com>
2025-11-08 12:47:52 -0600

    create header and set up .css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
   
 
   return (
     <>
-      
-      
+      <Header />
+      <h1>this is my app</h1>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+      <header>
+        <div className="header-content">
+          <img className="header-logo" src="src/assets/logo.png" alt="ForkCast Logo" />
+          <h1 className="header-title">ForkCast</h1>
+        </div>
+      </header>
+    );
+}
